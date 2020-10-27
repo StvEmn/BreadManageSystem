52e5e4dc470879088a3e0a5e6f19a0c8e1ccdde7
橘子味的猫° <2819836993@qq.com>
2020-10-27 12:58:29 +0800

    basic framework

---
+++ src/main/java/yihui/breadmanagersystem/BreadmanagersystemApplication.java
@@ -0,0 +1,13 @@
+package yihui.breadmanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadmanagersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreadmanagersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yihui/breadmanagersystem/BreadmanagersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package yihui.breadmanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadmanagersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
