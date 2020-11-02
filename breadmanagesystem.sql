6fc4b88c72c145bd28ef7d7da1f20df558b1641a
橘子味的猫° <2819836993@qq.com>
2020-11-02 20:08:34 +0800

    调整项目结构

--- breadmanagesystem.sql
+++ breadmanagesystem.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80021
  File Encoding         : 65001
 
- Date: 02/11/2020 19:21:04
+ Date: 02/11/2020 19:51:30
 */
 
 SET NAMES utf8mb4;
@@ -34,34 +34,28 @@ CREATE TABLE `purchase`  (
   `Address` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `Tel` varchar(11) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `CertificateNumber` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `certificate_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `goods_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `production_time` date NULL DEFAULT NULL,
-  `purchase_time` date NULL DEFAULT NULL,
-  `total_price` double NOT NULL,
-  `unit_price` double NOT NULL,
   PRIMARY KEY (`Id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of purchase
 -- ----------------------------
-INSERT INTO `purchase` VALUES (6, '苹果', 4, 10.00, 40.00, '斤', '2020-11-11', '2020-11-11', '内江师范学院', '内江市', '110', '10101010', NULL, NULL, NULL, NULL, 0, 0);
+INSERT INTO `purchase` VALUES (6, '苹果', 4, 10.00, 40.00, '斤', '2020-11-11', '2020-11-11', '内江师范学院', '内江市', '110', '10101010');
 
 -- ----------------------------
 -- Table structure for shipment
 -- ----------------------------
 DROP TABLE IF EXISTS `shipment`;
 CREATE TABLE `shipment`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `goods_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `quantity` int NULL DEFAULT 0,
-  `unit_price` decimal(5, 2) NULL DEFAULT 0.00,
-  `total_price` decimal(9, 2) NULL DEFAULT 0.00,
-  `specification` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `production_time` date NULL DEFAULT NULL,
-  `delivery_time` date NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `GoodsName` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `Quantity` int NULL DEFAULT 0,
+  `UnitPrice` decimal(5, 2) NULL DEFAULT 0.00,
+  `TotalPrice` decimal(9, 2) NULL DEFAULT 0.00,
+  `Specification` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `ProductionTime` date NULL DEFAULT NULL,
+  `DeliveryTime` date NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
